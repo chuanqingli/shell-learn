5f25b4cf2b526d0336ae25a6abbda2d9512d717b
chuanqingli <9915252@qq.com>
2018-09-27 05:29:18 +0800

    修改

--- or-test.sh
+++ or-test.sh
@@ -3,15 +3,14 @@
 知识点：
 or的表达式，这里只能用中括号;
 字符串变量用双引号括起(单引号不行);
+双中括号可以避免字符串变量转义的问题；
 
-字符串比较说明
-大多数时候，虽然可以不使用括起字符串和字符串变量的双引号，但这并不是好主意。为什么呢？因为如果环境变量中恰巧有一个空格或制表键，bash 将无法分辨，从而无法正常工作。
 COMMENT
 
 declare -a mountary=(mount "sda1 /" "sda3 /home" "0 /media/win/E" soft fstab)
 dotest(){
     for aaa in "${mountary[@]}";do
-        if [ "${aaa}" == fstab ] || [ "${aaa}" == soft ] ;then
+        if [[ ${aaa} == fstab ]] || [[ ${aaa} == soft ]] ;then
             echo "111=>" $aaa
         else
             echo "222=>" $aaa
