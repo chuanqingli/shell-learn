302e98810522724490c38b84e5e8479d1fd72b10
chuanqingli <9915252@qq.com>
2018-09-27 16:59:55 +0800

    修改：     eval-test.sh

--- eval-test.sh
+++ eval-test.sh
@@ -1,20 +1,27 @@
 #!/bin/bash
 declare -a mkfsary=("ext4 sda1 sda3" "swap sda2")
+# 指定的挂载
 declare -a mountary=("sda1 /" "sda3 /home" "0 /media/win/E")
+# 需要增加的挂载
 declare -a fstabary=("# /dev/sda5" "UUID=000B89830009F592 /media/win/E ntfs-3g defaults 0 0") #blkid查uuid
+# 自定义的安装命令
 declare -a softary=()
-
 # 操作所指向的磁盘
-declare -A diskmap=([cfdisk]="sda sdb sda" [grub]="sda")
+declare -A diskmap=([cfdisk]="sda sdb sda sdb" [grub]="sda")
 # 主机名和用户名 如连网用静态ip，需要增加nic、addr、gw、dns节点
 declare -A hostmap=([host]=chuanqing [user]=chuanqing)
 
 extend-eval(){
-    if [[ "${1}" == cfdisk ]];then
-	    str=${diskmap[cfdisk]}
+    if [[ $1 == cfdisk ]] || [[ $1 == grub ]] ;then
+	    str=${diskmap[$1]}
         ppp=($str)
+
+        comd0=$1
+        if [[ $1 == grub ]];then
+            comd0="grub-install --recheck"
+        fi
 	    for key in ${ppp[*]};do
-	        cfdisk /dev/"${key}"
+            ${comd0} /dev/"${key}"
 	    done
         return
     fi
@@ -39,8 +46,12 @@ extend-eval(){
     esac
 
     for str in "${aaa[@]}";do
-        if [ "${1}" == fstab ] || [ "${1}" == soft ] ;then
-            single-$1 "${str}"
+        if [[ $1 == fstab ]] || [[ $1 == soft ]] ;then
+            if [[ $1 == soft ]];then
+                $str
+                continue
+            fi
+            echo "$str">>/mnt/etc/fstab
             continue
         fi
 
@@ -62,11 +73,11 @@ single-mount(){
         return
     fi
 
-    if(($2!=/));then
+    if [[ $2 != / ]];then
         mkdir -p /mnt$2
     fi
 
-    if(($1==0));then
+    if [[ $1 == 0 ]];then
         return
     fi
     mount /dev/$1 /mnt$2
@@ -85,14 +96,7 @@ single-mkfs(){
     mkfs -t $1 /dev/$2
 }
 
-single-fstab(){
-    if [[ $# != 1 ]];then
-        return
-    fi
-    echo $1>>/mnt/etc/fstab
-}
-
-single-soft(){
+single-comd(){
     if [[ $# != 1 ]];then
         return
     fi
@@ -100,7 +104,7 @@ single-soft(){
 }
 
 #extend-eval cfdisk
-extend-eval mkfs
+extend-eval $1
 
 #extend-eval-main "${mkfsary[@]}"
 
